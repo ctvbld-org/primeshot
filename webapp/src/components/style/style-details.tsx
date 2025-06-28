82b7d60ef9a54a2584e1966fc725e9882d58f371
David <ctvbld@users.noreply.github.com>
2025-06-29 01:46:24 +0200

    Update CHANGELOG, refine animation easing type in style-details, and add new package dependencies

--- webapp/src/components/style/style-details.tsx
+++ webapp/src/components/style/style-details.tsx
@@ -53,7 +53,7 @@ const imageVariants = {
     opacity: 1,
     transition: {  
       duration: 0.5,
-      ease: [0.25, 0.1, 0.25, 1]
+      ease: [0.25, 0.1, 0.25, 1] as [number, number, number, number]
     }
   }
 }
@@ -136,7 +136,7 @@ export function StyleDetails({
         transition={{ 
           duration: 1,
           delay: 0,
-          ease: [0.25, 0.1, 0.25, 1]
+          ease: [0.25, 0.1, 0.25, 1] as [number, number, number, number]
         }}
       >
         <div className={styles.content}>
