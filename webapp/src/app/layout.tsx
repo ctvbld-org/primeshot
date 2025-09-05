7628e410776028a855f21cfa95cd09e04dea58b3
ctvbld <david@creativebuild.com>
2025-09-05 14:34:38 +0200

    refactor: consolidate layout providers into RootProviders component
    - Replaced multiple individual context providers in layout.tsx with a single RootProviders component for improved organization and readability.
    - Introduced RootProviders component to encapsulate all necessary providers, streamlining the layout structure and enhancing maintainability.

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -2,18 +2,7 @@ import { Inter } from 'next/font/google'
 import { carb } from '@/fonts'
 import './globals.css'
 
-import { QueryProvider } from '@/components/providers/query-provider'
-import { Toaster } from "@primeshot/common/web/ui/toaster"
-import { BannerProvider } from "@primeshot/common/web/ui/use-banner"
-import { Header } from '@primeshot/common'
-import { CreditsHeaderRight } from '@/components/header/CreditsHeaderRight'
-import { AuthProvider } from '@primeshot/common'
-import { LanguageProvider } from '@primeshot/common'
-import I18nProviderClient from '@/components/providers/I18nProvider'
-import { DialogServiceProvider } from '@/contexts/DialogServiceContext'
-import { IntentHandler } from '@/components/providers/intent-handler'
-import QueryParamCleaner from '@/components/shared/QueryParamCleaner'
-import { InferenceQueueProvider } from '@/contexts/inference-queue-context'
+import RootProviders from '@/components/providers/RootProviders'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,25 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${carb.variable} ${inter.className} dark`}>
-        <AuthProvider>
-          <I18nProviderClient>
-            <LanguageProvider>
-              <QueryProvider>
-                <BannerProvider>
-                  <DialogServiceProvider>
-                    <InferenceQueueProvider>
-                      <IntentHandler />
-                      <QueryParamCleaner />
-                      <Header rightSlot={<CreditsHeaderRight />} />
-                      {children}
-                      <Toaster />
-                    </InferenceQueueProvider>
-                  </DialogServiceProvider>
-                </BannerProvider>
-              </QueryProvider>
-            </LanguageProvider>
-          </I18nProviderClient>
-        </AuthProvider>
+        <RootProviders>
+          {children}
+        </RootProviders>
       </body>
     </html>
   )
