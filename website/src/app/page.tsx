58c5dab67bd7aa5addd82f02d8fccf35cd3fa7f2
ctvbld <david@creativebuild.com>
2025-09-22 10:12:29 +0200

    fix: update landing page image reference
    - Changed the image reference for landing-page-14 to landing-page-15 in the image loading logic to ensure the correct image is displayed.

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     process.env.NEXT_PUBLIC_AWS_DISTRIBUTION ? `${process.env.NEXT_PUBLIC_AWS_DISTRIBUTION}/website-images/landing-page-9-w1920.webp` : '/website-images/landing-page-9-w1920.webp',
     process.env.NEXT_PUBLIC_AWS_DISTRIBUTION ? `${process.env.NEXT_PUBLIC_AWS_DISTRIBUTION}/website-images/landing-page-10-w1920.webp` : '/website-images/landing-page-10-w1920.webp',
     process.env.NEXT_PUBLIC_AWS_DISTRIBUTION ? `${process.env.NEXT_PUBLIC_AWS_DISTRIBUTION}/website-images/landing-page-13-w1920.webp` : '/website-images/landing-page-10-w1920.webp',
-    process.env.NEXT_PUBLIC_AWS_DISTRIBUTION ? `${process.env.NEXT_PUBLIC_AWS_DISTRIBUTION}/website-images/landing-page-14-w1920.webp` : '/website-images/landing-page-10-w1920.webp',
+    process.env.NEXT_PUBLIC_AWS_DISTRIBUTION ? `${process.env.NEXT_PUBLIC_AWS_DISTRIBUTION}/website-images/landing-page-15-w1920.webp` : '/website-images/landing-page-10-w1920.webp',
   ];
 
   // Shuffle the images randomly on each page load
