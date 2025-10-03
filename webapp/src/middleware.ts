7467f6feeb1f767eb425f9b3105c26c43ebc42cd
ctvbld <david@creativebuild.com>
2025-10-03 12:24:12 +0200

    fix: update middleware matcher to exclude .well-known directory
    - Modified the middleware configuration to exclude the .well-known directory from processing, enhancing routing accuracy and security.

--- webapp/src/middleware.ts
+++ webapp/src/middleware.ts
@@ -75,6 +75,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|public).*)'
+    '/((?!api|_next/static|.well-known|_next/image|favicon.ico|public).*)'
   ]
 }
