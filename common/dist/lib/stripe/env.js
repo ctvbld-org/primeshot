9c523d886309d82e2d741406607a3a2b292993c2
ctvbld <david@creativebuild.com>
2025-10-03 09:21:24 +0200

    fix: update Stripe environment return value for staging
    - Changed the return value for the Stripe environment from 'production' to 'staging' in the environment configuration function.

--- common/dist/lib/stripe/env.js
+++ common/dist/lib/stripe/env.js
@@ -1,7 +1,7 @@
 export function getStripeEnv() {
     const env = typeof process !== 'undefined' ? process.env.NEXT_PUBLIC_VERCEL_TARGET_ENV : undefined;
     if (env === 'production')
-        return 'production';
+        return 'staging';
     if (env === 'staging')
         return 'staging';
     return 'dev';
